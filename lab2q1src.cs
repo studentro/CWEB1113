ddec02dfa2b6ffccb5b822964d06fdaf87cac1d7
studentro <90772786+studentro@users.noreply.github.com>
2022-04-06 21:14:08 -0500

    Add files via upload
    source code for lab 2

---
+++ lab2q1src.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab2Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 3;
+            Console.WriteLine("enter a number to test=" + myNum);
+
+            Console.WriteLine("Using multiply Assignment operator to multiply 5 to myNum...");
+            myNum *= 5;
+
+            Console.WriteLine("Updated Value of myNum=" + myNum);
+        }
+    }
+}
+ 
---
+++ lab2q2src.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab2Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = -2;
+            Console.WriteLine("enter a number to test=" + myNum);
+
+            Console.WriteLine("Using multiply Assignment operator to multiply 6 to myNum...");
+            myNum *= 6;
+
+            Console.WriteLine("Updated Value of myNum=" + myNum);
+        }
+    }
+}
